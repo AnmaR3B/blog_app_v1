6a67b70538acd93484755bd10ed77457a4d658d2
AnmaR <anmarma100@gmail.com>
2025-01-08 11:02:42 +0500

    Creating SEO Friendly URLs For Posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request,id):
+def post_detail(request,year,month,day,post):
     # #method 1 
     # try:
     #     post = Post.objects.get(id)
@@ -15,5 +15,9 @@ def post_detail(request,id):
     #     raise Http404("No post found")
     # return render(request, 'blog/post/detail.html', {'post':post})
     #method 2 
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                    slug=post,
+                    publish__year = year,
+                    publish__month=month,
+                    publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
